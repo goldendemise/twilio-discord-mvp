c0db46770724e33ff106190a7adbaca1dca20700
Sean <14907745+goldendemise@users.noreply.github.com>
2021-06-13 02:37:39 +0000

    Initial commit

---
+++ send_sms.rb
@@ -0,0 +1,16 @@
+# Download the twilio-ruby library from twilio.com/docs/libraries/ruby
+require 'twilio-ruby'
+require './creds'
+
+account_sid = TwilioCreds::ACCOUNT_ID
+auth_token = TwilioCreds::AUTHORIZATION_TOKEN
+client = Twilio::REST::Client.new(account_sid, auth_token)
+
+from = TwilioCreds::MY_TWILIO_NUMBER # Your Twilio number
+to = TwilioCreds::MY_PHONE_NUMBER # Your mobile phone number
+puts(TwilioCreds::ACCOUNT_ID)
+client.messages.create(
+from: from,
+to: to,
+body: "Hey friend!"
+)
